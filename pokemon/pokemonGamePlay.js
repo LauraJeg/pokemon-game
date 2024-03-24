0afea25d78a6717dc281ef1177a09e9c9a45bcbc
mashca <mashca@Xubuntu.myguest.virtualbox.org>
2024-03-24 22:28:15 +0000

    completed battle functionality

--- pokemon/pokemonGamePlay.js
+++ pokemon/pokemonGamePlay.js
@@ -73,15 +73,52 @@ function selectPokemon (){
             } ]
             )
     .then((answers)=> {
-      if (answers.trainerPokemon === '...empty'){
+      if (answers.yourPokemon === '...empty'){
         console.log("You cannot select an empty PokeBall")
         return selectPokemon ()
       }
       const yourPokemon= allPokemon.filter((pokemon)=> pokemon.name === answers.yourPokemon)[0];
       const oppPokemon =  allPokemon.filter((pokemon)=> pokemon.name === answers.oppPokemon)[0];
-      console.log(yourPokemon, oppPokemon)
-           const newBattle = new Battle(playerTrainerName, yourPokemon, oppPokemon.name, oppPokemon)});
-           
+      const newBattle = new Battle(playerTrainerName, yourPokemon, oppPokemon.name, oppPokemon)
+       battle(newBattle)
+    });
 };
 
+function battle (newBattle){
+
+
+  if (newBattle.hasBattleEnded) {
+    return inquirer.prompt(secondQuestions)
+        .then (answers => {
+          menuFunc(answers.question)});
+  }
+
+
+    inquirer
+      .prompt({
+        message:"What is your move",
+      type: "list",
+      name:"move",
+      choices:['Attack', 'Run away']
+      })
+      .then(({move}) => {
+              if (move === 'Attack') {
+                newBattle.fight();
+                newBattle.fight();
+                console.log(`${newBattle.pokemon1.name} has ${newBattle.pokemon1.hitPoints} hitpoints left`)
+                battle(newBattle)
+              } else {
+                console.log('You ran away!')
+                return inquirer.prompt(secondQuestions)
+                        .then (answers => {
+                          menuFunc(answers.question)});
+              }
+          })
+}
+
 playGame();
+
+// while (newBattle.hasBattleEnded === false){
+//   //blah blah blah
+// }
+// menuFunc()
